e1b4f7172ffb261639b0dd9a2832dd88c13e6322
Akshit Sahani <akshit_sahani@hotmail.com>
2017-01-31 22:03:53 -0500

    working website with all desired functions and stretch completed

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,14 @@
 module EventsHelper
+  def event_link(event)
+
+
+    result = "Name: #{event.name}<br>"
+    result += "Date: #{event.date}<br>"
+    result += "Capacity: #{event.capacity.to_i}"
+    link_to result.html_safe, location_event_path(@location, event)
+    # Name: <%= event.name %> <br>
+    # Date: <%= event.date %> <br>
+    # Capacity: <%= event.capacity %>
+    # <%= link_to "open " + event.name, location_event_path(@location, event) %> <br> <br>
+  end
 end
